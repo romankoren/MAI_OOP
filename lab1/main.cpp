5ceb099477321dc446f9bf6038ad65c414079b1b
sophistik <sophistik@users.noreply.github.com>
2018-09-28 14:14:19 +0300

    Add files via upload
    Added menu

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -3,25 +3,56 @@
 #include "square.h"
 #include "octagon.h"
 
+using namespace std;
+
+void print_menu(){
+		cout << "Выберие фигуру:" << endl;
+		cout << 1 << '\t' << "Восьмиугольник" << endl;
+		cout << 2 << '\t' << "Треугольник" << endl;
+		cout << 3 << '\t' << "Квадрат" << endl;
+	}
+
 int main(int argc, char** argv) {
+	int c;
+	Figure *ptr;
+	print_menu();
+	
+	while (cin >> c){
+		
+			
+		switch (c){
+			case 1:
+				
+				cout << "Введите сторону правильного восьмиугольника" << endl;
+				ptr = new Octagon(cin);
+				ptr->Print();
+				cout << "S = " << ptr->Square() << endl;
+				delete ptr;
+				break;
+				
+			case 2:
+			
+				cout << "Введите стороны треугольника" << endl;
+				ptr = new Triangle(cin);
+				ptr->Print();
+				cout << "S = " << ptr->Square() << endl;
+				delete ptr;
+				break;
+			case 3:
 
-    std::cout << "Введите стороны треугольника" << std::endl;
-    Figure *ptr = new Triangle(std::cin);
-    ptr->Print();
-    std::cout << "S = " << ptr->Square() << std::endl;
-    delete ptr;
-
-    std::cout << "Введите сторону квадрата" << std::endl;
-    ptr = new FSquare(std::cin);
-    ptr->Print();
-    std::cout << "S = " << ptr->Square() << std::endl;
-    delete ptr;
-
-    std::cout << "Введите сторону правильного восьмиугольника" << std::endl;
-    ptr = new Octagon(std::cin);
-    ptr->Print();
-    std::cout << "S = " << ptr->Square() << std::endl;
-    delete ptr;
+				cout << "Введите сторону квадрата" << endl;
+				ptr = new FSquare(cin);
+				ptr->Print();
+				cout << "S = " << ptr->Square() << endl;
+				delete ptr;
+				break;
+			
+			default:
+				print_menu();
+				
+		}
+	}
+			
 
     return 0;
 }
